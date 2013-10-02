315b440ce772fff226f59bfb4c71ced48a105f5f
Whispe <tyung1@jhu.edu>
2013-10-01 22:17:43 -0400

    Update SAS.java
    Submission 1. HLT?

--- SAS.java
+++ SAS.java
@@ -1,21 +1,21 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
-
 /*
  * Tifany Yung
  * tyung1@jhu.edu
  * Assignment 3
  * October 2, 2013
- * USAGE
+ *
+ * USAGE: Please place SAS.java and input file into the same directory first.
  * To compile: javac SAS.java
  * To run: java SAS SAS.class < loop.s (or loop.z)
  */
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
  * Class to generate a .scram file.
- *
  */
 public final class SAS {
 
@@ -27,57 +27,61 @@ public final class SAS {
     static final int EIGHT = 8;
     static final int MAX_OPERATIONS = 16;
     static final int MAX_DATA_WIDTH = 256;
+    // A HashMap mapping OPCODES to their binary representation.
+    static final HashMap<String, Integer> OPCODES =
+            new HashMap<String, Integer>();
+
+    HashMap<String, Integer> labelToOpNum;
+    FileOutputStream outFile;
+    Scanner fileIn;
+    String line; // Current line being reading (trimmed).
+    String holdLine; // Current line being readm(uncut).
+    String[] lineFrags;
+    String[] storeLines;
+    int[] storeLineNums;
 
     /**
      * Default constructor.
      */
-    private SAS() {}
+    private SAS(FileOutputStream stream, Scanner scanner) throws IOException {
+
+        this.line = "";
+        this.holdLine = "";
+        this.storeLines = new String[MAX_OPERATIONS];
+        this.storeLineNums = new int[MAX_OPERATIONS];
+        this.labelToOpNum = new HashMap<String, Integer>();
+        this.outFile = new FileOutputStream("loop.scram");
+        this.fileIn = new Scanner(System.in);
+
+        OPCODES.put("DAT", 0);
+        OPCODES.put("LDA", 1);
+        OPCODES.put("LDI", 2);
+        OPCODES.put("STA", THREE);
+        OPCODES.put("STI", FOUR);
+        OPCODES.put("ADD", FIVE);
+        OPCODES.put("SUB", SIX);
+        OPCODES.put("JMP", SEVEN);
+        OPCODES.put("JMZ", EIGHT);
+    }
 
     /**
-    * Generates .scram file from loop.s or (.z) with opcodesToByte and operands.
+    * Generates .scram file from loop.s or (.z) with OPCODES and operands.
     * @param args the arguments.
     * @throws IOException the exception.
     */
     public static void main(String[] args) throws IOException {
 
-        // A FileOutputStream to write the loop.scram file.
-        FileOutputStream outFile = new FileOutputStream("loop.scram");
-        // A Scanner to read input from loop.s or loop.z.
-        Scanner fileIn = new Scanner(System.in);
+        SAS sas = new SAS(new FileOutputStream("loop.scram"),
+            new Scanner(System.in));
 
-        String line = ""; // Current line being reading (trimmed).
-        String holdLine = ""; // Current line being readm(uncut).
-        String[] lineFrags;
-        String[] storeLines = new String[MAX_OPERATIONS];
-        int[] storeLineNums = new int[MAX_OPERATIONS];
-
-        boolean isData = false;
         int lineNum = 1; // Current number of the line being read.
         int numOperations = 0;
-        int holdAddress = 0;
-        int holdOpcodeByte = 0;
-        byte opByte = 0;
-
-        // A HashMap mapping opcodesToByte to their binary representation.
-        HashMap<String, Integer> opcodesToByte = new HashMap<String, Integer>();
-
-        opcodesToByte.put("DAT", 0);
-        opcodesToByte.put("LDA", 1);
-        opcodesToByte.put("LDI", 2);
-        opcodesToByte.put("STA", THREE);
-        opcodesToByte.put("STI", FOUR);
-        opcodesToByte.put("ADD", FIVE);
-        opcodesToByte.put("SUB", SIX);
-        opcodesToByte.put("JMP", SEVEN);
-        opcodesToByte.put("JMZ", EIGHT);
-
-        HashMap<String, Integer> labelToOpNum = new HashMap<String, Integer>();
 
         //First pass.
-        while (fileIn.hasNext()) {
+        while (sas.fileIn.hasNext()) {
 
             lineNum++;
-            holdLine = fileIn.nextLine();
+            sas.holdLine = sas.fileIn.nextLine();
             if (numOperations > MAX_OPERATIONS) {
 
                 System.out.print("Maximum number of operations exceeds memor");
@@ -86,37 +90,38 @@ public final class SAS {
             }
 
             // Split current line into label/instruction/operand and comments.
-            lineFrags = holdLine.split("#", 2);
+            sas.lineFrags = sas.holdLine.split("#", 2);
 
-            if (!lineFrags[0].isEmpty()) {
+            if (!sas.lineFrags[0].isEmpty()) {
 
-                line = lineFrags[0].trim(); // Save label/instruction/operand.
+                // Save label/instruction/operand.
+                sas.line = sas.lineFrags[0].trim();
                 // Attempt to separate label and instruction/operand.
-                lineFrags = line.split(":", 2);
+                sas.lineFrags = sas.line.split(":", 2);
 
-                if (lineFrags.length > 1) { // If there's a label.
+                if (sas.lineFrags.length > 1) { // If there's a label.
 
                     // Check if the label was defined twice.
-                    if (labelToOpNum.containsKey(lineFrags[0])) {
+                    if (sas.labelToOpNum.containsKey(sas.lineFrags[0])) {
 
                         System.out.print("Label was repeated at line ");
                         System.out.println(lineNum);
                         System.exit(0);
                     } else {
 
-                        labelToOpNum.put(lineFrags[0], numOperations);
+                        sas.labelToOpNum.put(sas.lineFrags[0], numOperations);
                     }
 
-                    if (!lineFrags[1].isEmpty()) { // If more than a label.
+                    if (!sas.lineFrags[1].isEmpty()) { // If more than a label.
 
-                        storeLines[numOperations] = lineFrags[1].trim();
-                        storeLineNums[numOperations] = lineNum;
+                        sas.storeLines[numOperations] = sas.lineFrags[1].trim();
+                        sas.storeLineNums[numOperations] = lineNum;
                         numOperations++;
                     }
                 } else {
 
-                    storeLines[numOperations] = lineFrags[0].trim();
-                    storeLineNums[numOperations] = lineNum;
+                    sas.storeLines[numOperations] = sas.lineFrags[0].trim();
+                    sas.storeLineNums[numOperations] = lineNum;
                     numOperations++;
                 }
             }
@@ -126,64 +131,94 @@ public final class SAS {
         // Pass two.
         for (int i = 0; i < MAX_OPERATIONS; i++) {
 
-            line = storeLines[i];
-            if (line == null) {
+            sas.line = sas.storeLines[i];
+            if (sas.line == null) {
                 break;
             }
-            lineFrags = line.split("\t", 2);
-            System.out.println(lineFrags[0]);
-            //System.out.println(lineFrags[1]);
+            sas.lineFrags = sas.line.split("\\s", 2);
+            sas.outFile.write(calculateByte(sas, i));
+        }
+        sas.outFile.close();
+    }
 
-            for (int j = 0; j < lineFrags.length; j++) {
+    /**
+     * Calculates the byte to write to the .scram file.
+     * @param sas the SAS instance.
+     * @param ln the current line number.
+     * @return the byte to write.
+     */
+    static byte calculateByte(SAS sas, int ln) {
 
-                if (!lineFrags[j].trim().isEmpty()) {
+        byte retByte = 0;
+        boolean isData = false;
+        int holdAddress = 0;
+        int holdOpcodeByte = 0;
 
-                    if (opcodesToByte.containsKey(lineFrags[j])) { // Is opcode.
+        for (int j = 0; j < sas.lineFrags.length; j++) {
 
-                        holdOpcodeByte = opcodesToByte.get(lineFrags[j]);
-                        if (lineFrags[j].equals("DAT")) {
+            if (!sas.lineFrags[j].trim().isEmpty()) {
 
-                            isData = true;
-                        }
+                if (OPCODES.containsKey(sas.lineFrags[j])) { // Is opcode.
 
-                        opByte = (byte) (holdOpcodeByte << FOUR);
-                    } else { // Probably an address, either numeric or a label.
+                    holdOpcodeByte = OPCODES.get(sas.lineFrags[j]);
+                    if (sas.lineFrags[j].equals("DAT")) {
 
-                        if (isNumeric(lineFrags[j])) { // If it's a number.
-
-                            holdAddress = Integer.parseInt(lineFrags[j]);
-
-                            if (holdAddress > MAX_OPERATIONS && !isData) {
-
-                                System.out.print("Address cannot exceed 15 ");
-                                System.out.print("at line ");
-                                System.out.println(storeLineNums[i]);
-                                System.exit(0);
-                            } else if (holdAddress > MAX_DATA_WIDTH
-                                && isData) {
-
-                                System.out.print("Data cannot exceed ");
-                                System.out.print("255 at line ");
-                                System.out.println(storeLineNums[i]);
-                                System.exit(0);
-                            }
-                        } else if (labelToOpNum.containsKey(lineFrags[j])) {
-                            // If it's a label.
-                            holdAddress = labelToOpNum.get(lineFrags[j]);
-                        } else {
-
-                            System.out.print("Invalid address at line ");
-                            System.out.println(labelToOpNum.get(lineFrags[j]));
-                            System.exit(0);
-                        }
-
-                        opByte = (byte) (opByte | (byte) holdAddress);
+                        isData = true;
                     }
+
+                    retByte = (byte) (holdOpcodeByte << FOUR);
+                } else { // Probably an address, either numeric or a label.
+
+                    if (isNumeric(sas.lineFrags[j])) { // If it's a number.
+
+                        holdAddress = Integer.parseInt(sas.lineFrags[j]);
+                        checkAddressSize(sas, holdAddress, ln, isData);
+
+                    } else if (sas.labelToOpNum.containsKey(sas.lineFrags[j])) {
+                        // If it's a label.
+                        holdAddress = sas.labelToOpNum.get(sas.lineFrags[j]);
+                    } else {
+
+                        System.out.print("Invalid address at line ");
+                        System.out.println(
+                            sas.labelToOpNum.get(sas.lineFrags[j]));
+                        System.exit(0);
+                    }
+
+                    System.out.println(retByte);
+                    System.out.println((byte) holdAddress);
+                    retByte = (byte) (retByte | (byte) holdAddress);
+                    System.out.println(retByte);
                 }
             }
-            outFile.write(opByte);
         }
-        outFile.close();
+        return retByte;
+    }
+
+    /**
+     * Checks whether the address size is small enough; closes program if not.
+     * If the address is data, the address may take a value up to 255.
+     * If the address is not data, the address may take a value up to 15.
+     * @param sas the SAS instance.
+     * @param addr the address to check.
+     * @param ln the current line number.
+     * @param boolData true if the address is actually data, false otherwise.
+     */
+    static void checkAddressSize(SAS sas, int addr, int ln, boolean boolData) {
+
+        if (addr > MAX_OPERATIONS && !boolData) {
+
+            System.out.print("Address cannot exceed 15 ");
+            System.out.print("at line ");
+            System.out.println(sas.storeLineNums[ln]);
+            System.exit(0);
+        } else if (addr > MAX_DATA_WIDTH && boolData) {
+
+            System.out.print("Data cannot exceed ");
+            System.out.print("255 at line ");
+            System.out.println(sas.storeLineNums[ln]);
+            System.exit(0);
+        }
     }
 
     /**
